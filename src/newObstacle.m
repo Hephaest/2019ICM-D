959bd62fff78127a8c1a65323c35d3d154f1a143
Miao Cai <philliphily@gmail.com>
2019-07-19 02:44:37 -0400

    Update newObstacle.m

--- src/newObstacle.m
+++ src/newObstacle.m
@@ -1,12 +1,15 @@
 function [passage, flag] = newObstacle(N, passage, flag, scount, i, L)
 %
-% newObstacle   produce new obstacle. 
+% newObstacle   Produce new obstacle. 
+% 
 % Obstacles are distributed randomly on the passageway. 
 % Suppose obstacles could be removed or changed in a period of time.
 %
+% Author: Hephaest
+% July 18, 2019
 
 if mod(i, scount/10) == 0       % Obstacles will be changed. 
-    cnt = find(passage == -2);  %in every scount/10 time.
+    cnt = find(passage == -2);  % In every scount/10 time.
     passage(cnt) = 0;
     flag = 0;
 end
@@ -17,7 +20,7 @@ if flag == 0
     [row, col] = size([x, y])
     obstacle = [x, y];
     rand_num = ceil(rand * N);          % The number of obstacles is random.
-    select = randperm(row, rand_num); % The positions of obstacles are random.
+    select = randperm(row, rand_num);   % The positions of obstacles are random.
     obs = obstacle(select, :);
     [r, l] = size(obstacle(select, :));    
     for i = 1:r
